7fa5557ec6ec76603092d0a117f6ee01e16a778f
Luigi Machado <luigi.mach126@gmail.com>
2024-03-03 23:11:31 -0300

    Adição de comentarios e edição de parametros
    Adicionados comentarios em todas (ou quase) as funções do arquivo. Novos
    parametros nas funções de inserção

--- arvorern.h
+++ arvorern.h
@@ -26,37 +26,54 @@ cor_t corDoNO(NO *node);
 // Dado um nó, realiza a mudança de cor.
 // Caso seja Preto altera para vermelho,
 // caso seja vermelho altera para preto
-bool trocaCor(NO *node); 
+static bool trocaCor(NO *node); 
 
 // Aloca um novo nó a ser inserido
-NO* criarNo(item_t dados, NO* pai);
+static NO* criarNo(item_t dados, NO* pai);
 
-static bool ehFolha(NO* node);
+// Retorna true se o nó não tiver nenhum filho, caso contrario retorna false.
+bool ehFolha(NO* node);
 
+// Busca o menor nó a direita 
 NO* sucessorImediato(NO* node);
 
+// Deixa a arvore pronta para uso;
 void inicializar(arvore_rn *arvore);
 
+// Busca um nó na arvore e retorna um ponteiro para ele.
+// Caso não encontre retorna NULL
 NO* encontrarNO(arvore_rn *arvore, item_t chave);
 
 // Inserção padrão de arvore binária de busca;
 // FUNÇÃO ANTIGA
 static NO* old_inserir_bin(NO *raiz, item_t chave);
 
-// Inserção padrão de arvore binária de busca
-static bool inserir_bin(arvore_rn *arvore, item_t chave);
+// Inserção padrão de arvore binária de busca 
+static bool inserir_bin(arvore_rn *arvore, item_t chave, NO** novoNO_ptr);
 
+// Insere o nó na arvore_rn e realiza o balanceamento.
+// Retorna false caso ocorra algum erro na inserção ou se o nó já estiver presente.
 bool inserir(arvore_rn *arvore, item_t chave);
 
+// Muda o valor da chave de um nó para o valor de outro 
 static void trocarChave(NO* atual, NO* novo);
 
 // Remoção padrao de arvore binaria de busca
-bool remover_bin(arvore_rn *arvore, item_t chave);
+static bool remover_bin(arvore_rn *arvore, item_t chave);
 
+// Remove o nó da arvore_rn e realiza o balanceamento.
+// Retorna true caso remova e false se o nó não for encontrado
 bool remover(arvore_rn *arvore, item_t chave);
 
-void balancearArvore(arvore_rn *arvore);
+void balancearInsercao(arvore_rn *arvore, NO* novoNO);
 
-void inorder(NO* raiz);
+void balancearRemocao(arvore_rn *arvore);
+
+// Realiza navegação inOrder, exibindo todos os elementos da arvore.
+// Função base para a inorder 
+static void inorder_base(NO* raiz);
+
+// Realiza navegação inOrder, exibindo todos os elementos da arvore 
+void inorder(arvore_rn *arvore);
 
 #endif // ARVORE_RN 
